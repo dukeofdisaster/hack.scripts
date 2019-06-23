e5c7dd2997c7a8d2c1d2b5469c576b634bccf22e
root <root@localhost.localdomain>
2019-06-23 01:12:10 -0400

    fixed variables in mscan.sh

--- mscan.sh
+++ mscan.sh
@@ -4,6 +4,6 @@ read toscan
 echo "Enter interface to scan on"
 read interf
 masscan -p1-65535,U1:65535 $toscan --rate=1000 -e $interf > ports.txt
-ports=$(cat ports | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')
-nmap -Pn -sV -sC -p$ports toscan > nmap-out.txt
+ports=$(cat ports.txt | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')
+nmap -Pn -sV -sC -p$ports $toscan > nmap-out.txt
 echo "Scan complete, see ports.txt,nmap-out.txt"
